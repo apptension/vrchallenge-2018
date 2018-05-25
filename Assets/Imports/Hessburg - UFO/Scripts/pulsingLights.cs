5c6f916328ba3605c236a3deceee153a997c3edc
Patryk Ziemkowski <pziemkowski@apptension.com>
2018-05-25 17:12:43 +0200

    Add UFO and animals assets

---
+++ Assets/Imports/Hessburg - UFO/Scripts/pulsingLights.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pulsingLights : MonoBehaviour 
+{
+	void Start () 
+	{
+		if(ufoMaterial)
+		{	
+			ufoMaterial.EnableKeyword("_EMISSION");
+			saveOriginalColor=ufoMaterial.GetColor("_EmissionColor");
+		}
+	}
+	
+
+	void Update () 
+	{
+	 	if(ufoMaterial)
+		{
+			ufoLightIntensity=Mathf.PingPong(Time.time*0.5f, 0.8f);
+			ufoLightIntensity+=0.2f;
+			ufoMaterial.SetColor("_EmissionColor", new Color(ufoLightIntensity, ufoLightIntensity, ufoLightIntensity, 1.0f));
+		}
+	}
+
+	void OnApplicationQuit()
+	{
+		if(ufoMaterial) ufoMaterial.SetColor("_EmissionColor", saveOriginalColor);
+	}	
+
+	public Material ufoMaterial;
+	private float ufoLightIntensity;
+	private Color saveOriginalColor;
+}
+
---
+++ Assets/Imports/Low Poly Animated Animals/- Scripts/AnimalScripts/AudioManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LowPolyAnimalPack
+{
+  public class AudioManager : MonoBehaviour
+  {
+    private static AudioManager instance;
+
+    [SerializeField]
+    private float soundDistance = 7f;
+
+    [SerializeField]
+    private bool logSounds = false;
+
+    private void Awake()
+    {
+      instance = this;
+    }
+
+    public static void PlaySound(AudioClip clip, Vector3 pos)
+    {
+      if (!instance)
+      {
+        Debug.LogError("No Audio Manager found in the scene.");
+        return;
+      }
+
+      if (!clip)
+      {
+        Debug.LogError("Clip is null");
+        return;
+      }
+
+      if (instance.logSounds)
+      {
+        Debug.Log("Playing Audio: " + clip.name);
+      }
+
+      GameObject soundObject = new GameObject();
+      soundObject.transform.position = pos;
+      soundObject.name = "Sound";
+      AudioSource audioSource = soundObject.AddComponent<AudioSource>();
+      audioSource.spatialBlend = 1f;
+      audioSource.minDistance = instance.soundDistance;
+      audioSource.clip = clip;
+      audioSource.Play();
+      Destroy(soundObject, clip.length);
+    }
+  }
+}
