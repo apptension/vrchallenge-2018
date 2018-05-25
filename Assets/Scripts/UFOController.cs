78fe1a4a1f2b42f70cd8ca41a7c9a1a9f2920105
Michał Soczyński <soczynski.michal@gmail.com>
2018-05-25 19:48:54 +0200

    work with ufo

---
+++ Assets/Scripts/UFOController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class UFOController : MonoBehaviour {
+
+    private Component m_LastPlacedAnchor = null;
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+
+        if (m_LastPlacedAnchor != null)
+        {
+            // Instantiate Andy model at the hit pose.
+            var andyObject = Instantiate(_GetAndyPrefab(), m_LastPlacedAnchor.transform.position,
+                m_LastPlacedAnchor.transform.rotation);
+
+            // Make Andy model a child of the anchor.
+            andyObject.transform.parent = m_LastPlacedAnchor.transform;
+
+            // Save cloud anchor.
+            _HostLastPlacedAnchor();
+        }
+	}
+}
