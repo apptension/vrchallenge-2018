174a6675f03de84e639e81accc92e2752a0baa72
Michał Przyszczypkowski <michal@unit9.com>
2018-05-25 20:29:16 +0200

    Add RunAwayController for cows

---
+++ Assets/Scripts/RunAwayController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunAwayController : MonoBehaviour {
+    public float runSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay(Collider other) {
+        if (other.tag == "Wolf") {
+            Debug.Log("Oh no! It's the wolf!");
+
+            var cowTransform = transform.parent;
+            this.GetComponentInParent< AnimationControl > ().SetAnimation("isRunning");
+            var runDirection = (cowTransform.position - other.transform.position).normalized;
+
+            cowTransform.Translate(runDirection * this.runSpeed * Time.deltaTime, Space.World);
+            cowTransform.LookAt(runDirection + cowTransform.position);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Uff. Wolf is gone.");
+        this.GetComponentInParent<AnimationControl>().SetAnimationIdle();
+    }
+}
