6b802a2227d2f5c0780a69ab45062d828e1af89d
Michał Soczyński <soczynski.michal@gmail.com>
2018-05-26 18:09:24 +0200

    XXX

---
+++ Assets/Scripts/UFOCatcher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOCatcher : MonoBehaviour {
+    public UFOController UFOController;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Target" && UFOController.isCatching)
+        {
+            Destroy(other.gameObject);
+            Debug.Log("o, milka!");
+        }
+    }
+}
