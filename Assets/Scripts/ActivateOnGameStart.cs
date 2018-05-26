a2e52e6f9989156b561279713b5cb100fd6f4449
Michał Przyszczypkowski <michal@unit9.com>
2018-05-27 00:22:18 +0200

    Activate move pointer on game start

---
+++ Assets/Scripts/ActivateOnGameStart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateOnGameStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.SetActive(false);
+        GameManager.instance.GameStarted += HandleGameStarted;
+	}
+
+    void HandleGameStarted(object sender, System.EventArgs e)
+    {
+        gameObject.SetActive(true);
+    }
+}
