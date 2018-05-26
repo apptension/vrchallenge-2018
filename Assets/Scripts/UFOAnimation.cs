066ebaffd85672354c6339de8273cad54f7068d9
Michał Soczyński <soczynski.michal@gmail.com>
2018-05-26 13:43:36 +0200

    Add ufo button

---
+++ Assets/Scripts/UFOAnimation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOAnimation : MonoBehaviour {
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(0,  60 * Time.deltaTime, 0, Space.World);
+	}
+}
